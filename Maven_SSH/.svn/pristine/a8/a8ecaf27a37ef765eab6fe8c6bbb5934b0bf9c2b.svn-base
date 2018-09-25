5b2e0126061a3ef4fca16685ce73e6ef47aadda5
pengxu0118 <1475819314@qq.com>
2018-09-25 10:20:06 +0800

    delete

--- Maven_SSH/.svn/pristine/a8/a8ecaf27a37ef765eab6fe8c6bbb5934b0bf9c2b.svn-base
+++
@@ -1,7 +0,0 @@
-package cn.itcast.dao;
-
-import cn.itcast.domain.Region;
-
-public interface UtilsDao extends BaseDao<Region>{
-
-}
--- Maven_SSH/src/main/java/cn/itcast/dao/impl/UtilsDaoImpl.java
+++
@@ -1,7 +0,0 @@
-package cn.itcast.dao.impl;
-
-import cn.itcast.dao.UtilsDao;
-import cn.itcast.domain.Region;
-
-public class UtilsDaoImpl extends BaseDaoImpl<Region> implements UtilsDao {
-}
